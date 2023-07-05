846d87989e40fdb981e367872b59490a0032a2e9
Bagas Hizbullah <work.bitsy@simplelogin.com>
2023-07-05 20:52:42 +0700

    fix(booking): fix booked seats tracking error

--- app/Models/Seat.php
+++ app/Models/Seat.php
@@ -41,19 +41,22 @@ class Seat extends Model
     }
 
     /**
-     * Check if the seat is booked.
+     * Check if the seat is booked for a specific movie.
      *
+     * @param Movie $movie
      * @param Date $date
      * @param Showtime $showtime
      * @return bool
      */
-    public function isBooked(Date $date, Showtime $showtime): bool
+    public function isBooked(Movie $movie, Date $date, Showtime $showtime): bool
     {
         return DB::table('booked_seats')
             ->join('date_showtime', 'booked_seats.date_showtime_id', '=', 'date_showtime.id')
+            ->join('bookings', 'booked_seats.booking_id', '=', 'bookings.id')
             ->where('date_showtime.date_id', $date->id)
             ->where('date_showtime.showtime_id', $showtime->id)
             ->where('booked_seats.seat_id', $this->id)
+            ->where('bookings.movie_id', $movie->id)
             ->exists();
     }
 }
