c6c0b7b001664c125341f1243adf285648b9988f
Bagas Hizbullah <work.bitsy@simplelogin.com>
2023-07-06 10:20:15 +0000

    fix(db): fix db rollback migration (#24)

--- database/migrations/2023_06_28_104800_create_booked_seats_table.php
+++ database/migrations/2023_06_28_104800_create_booked_seats_table.php
@@ -29,6 +29,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('booked_seat');
+        Schema::dropIfExists('booked_seats');
     }
 };
